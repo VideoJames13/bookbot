b3c00ccd75804ced4f34545d38508f06e8f2a0c3
VideoJames13 <JamesMehran1@gmail.com>
2025-05-14 22:58:51 -0400

    updated main.py to print the contents of frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+def get_book_text(file):
+    with open(file) as f:
+        file_contents = f.read()
+        return file_contents
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+main()
