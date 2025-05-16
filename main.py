21c65cd8b6bccb56dcf8aef3ab87605ca24f1ea7
VideoJames13 <JamesMehran1@gmail.com>
2025-05-16 14:17:02 -0400

    refactor - separated the counter function into stats.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ def get_book_text(file):
     with open(file) as f:
         file_contents = f.read()
         return file_contents
-def word_count(file_contents):
-    count = len(file_contents.split())
-    return count
+
+from stats import word_count
 
 def main():
     contents = get_book_text("./books/frankenstein.txt")
