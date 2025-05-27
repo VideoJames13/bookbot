3465699739c598a45908d97bce15c101a4e2e086
VideoJames13 <JamesMehran1@gmail.com>
2025-05-27 15:02:26 -0400

    main.py now accepts the filepath as argument, and kicks back an error if user doesn't include it

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+#takes the file and returns all the text within
 def get_book_text(file):
     with open(file) as f:
         file_contents = f.read()
@@ -9,16 +12,19 @@ from stats import character_count
 
 from stats import list_sort
 
-#fake comment for streak!
-
+#this is where it all comes together
 def main():
-    contents = get_book_text("./books/frankenstein.txt")
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count(contents)} total words")
-    print("--------- Character Count -------")
-    for dict in list_sort(character_count(contents)):
-        print(f"{dict["char"]}: {dict["num"]}")
+    if len(sys.argv) == 2:
+        contents = get_book_text(sys.argv[1])
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {word_count(contents)} total words")
+        print("--------- Character Count -------")
+        for dict in list_sort(character_count(contents)):
+            print(f"{dict["char"]}: {dict["num"]}")
+    else:
+        print("Hey dummy, Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
